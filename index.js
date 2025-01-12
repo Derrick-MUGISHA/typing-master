143c1691e4ce7382c414d0bdfa21551a824ee7b0
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2025-01-12 13:22:05 +0200

    much worlds

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let isTyping = false;
 
 async function fetchRandomWords() {
     try {
-        const response = await fetch("https://random-word-api.herokuapp.com/word?number=20");
+        const response = await fetch("https://random-word-api.herokuapp.com/word?number=200");
         const words = await response.json();
         return words.join(" "); // Combine words into a sentence
     } catch (error) {
